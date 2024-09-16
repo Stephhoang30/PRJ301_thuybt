8e4506d90cf284c3204f995f27993e3d739aea92
stephHoang30 <steph30forwork@gmail.com>
2024-09-16 23:31:01 +0700

    init ProductDAO and CategoryDAO

---
+++ Team5_HE172214_Assignment/src/java/dal/implement/CategoryDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal.implement;
+
+import dal.DBContext;
+
+/**
+ *
+ * @author stephhoang
+ */
+public class CategoryDAO extends DBContext{
+    
+}
